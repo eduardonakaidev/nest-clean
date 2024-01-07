ca96db76a81a3de45d83fc0ec0881da0c6a1d0ef
eduardo i nakai frasson <109884797+eduardonakaidev@users.noreply.github.com>
2024-01-07 17:39:27 -0300

    comecei o projeto, já foi criado a aparte de criar o usuario e validações de body, implementado o prisma e o eslint

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,64 @@
+import {
+  ConflictException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+} from '@nestjs/common'
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+
+// definindo schema do body da requisição
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+// tranferindo o schema para um typeof
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+// declaração que é um controller
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+  // definindo rota post
+  @Post()
+
+  // definindo o retorno positivo caso seja concluido
+  @HttpCode(201)
+
+  // adicionando validação no body
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    // declaração do body
+    const { name, email, password } = body
+
+    // verificando se o email já existe no banco de dados
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      )
+    }
+
+    // hash da senha enviada
+    const hashedPassword = await hash(password, 8)
+
+    // criando no banco
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
